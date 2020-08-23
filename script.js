4bf8ddba942774df87371b40f94f03fbb3820214
dipranjanJS <dipranjan16oct@gmail.com>
2020-08-23 21:41:55 +0530

    picture in picture functionality

---
+++ script.js
@@ -0,0 +1,30 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt user to select media stream, pass to video element and play
+
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            console.log("loaded");
+            videoElement.play();
+        }
+    } catch(error) {
+        // Catch error here
+        console.log('Whoops, error here:', error);
+    }
+}
+
+button,addEventListener('click', async () => {
+    // Disable Button
+    button.disabled = true;
+    // Start picture in picture
+    await videoElement.requestPictureInPicture();
+    // Reset the button
+    button.disabled = false;
+});
+
+// On load
+selectMediaStream()
